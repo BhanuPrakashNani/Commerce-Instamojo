6a3907d7a46465f08b4cfd5a4e403ab7db27ef32
bhanuprakashnani <p.bhanuprakash12345@gmail.com>
2018-06-11 18:25:03 +0530

    creates payment request

---
+++ Forms/createpayment.php
@@ -0,0 +1,14 @@
+<?php
+try {
+    $response = $api->paymentRequestCreate(array(
+        "purpose" => "FIFA 16",
+        "amount" => "3499",
+        "send_email" => true,
+        "email" => "foo@example.com",
+        "redirect_url" => "http://www.example.com/handle_redirect.php"
+        ));
+    print_r($response);
+}
+catch (Exception $e) {
+    print('Error: ' . $e->getMessage());
+}
