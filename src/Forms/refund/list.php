fa8bbee71d59fd2cfdcd703b9eae683e87e64cea
bhanuprakashnani <p.bhanuprakash12345@gmail.com>
2018-06-13 14:38:22 +0530

    moved to src

---
+++ src/Forms/refund/list.php
@@ -0,0 +1,15 @@
+{
+  "refunds": [
+    {
+      "id": "C5c0751269",
+      "payment_id": "MOJO5a06005J21512197",
+      "status": "Refunded",
+      "type": "QFL",
+      "body": "Customer isn't satisfied with the quality",
+      "refund_amount": "2500.00",
+      "total_amount": "2500.00",
+      "created_at": "2015-12-07T11:01:37.640Z"
+    }
+   ]
+  "success": true
+}
