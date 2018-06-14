b81d021866e044ff95bc9be5343d188c9def2602
Bhanu Prakash Poluparthi <32234113+BhanuPrakashNani@users.noreply.github.com>
2018-06-14 14:59:04 +0530

    Update listpayment.php

--- src/Forms/listpayment.php
+++ src/Forms/listpayment.php
@@ -1,9 +1,29 @@
 <?php
 
-try {
-    $response = $api->paymentRequestsList();
-    print_r($response);
-}
-catch (Exception $e) {
-    print('Error: ' . $e->getMessage());
-}
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://www.instamojo.com/api/1.1/payment-requests/');
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER,
+            array("X-Api-Key:d82016f839e13cd0a79afc0ef5b288b3",
+                  "X-Auth-Token:3827881f669c11e8dad8a023fd1108c2"));
+$payload = Array(
+    'purpose' => 'FIFA 16',
+    'amount' => '2500',
+    'phone' => '9999999999',
+    'buyer_name' => 'John Doe',
+    'redirect_url' => 'http://www.example.com/redirect/',
+    'send_email' => true,
+    'webhook' => 'http://www.example.com/webhook/',
+    'send_sms' => true,
+    'email' => 'foo@example.com',
+    'allow_repeated_payments' => false
+);
+$response = curl_exec($ch);
+curl_close($ch); 
+
+echo $response;
+
+?>
