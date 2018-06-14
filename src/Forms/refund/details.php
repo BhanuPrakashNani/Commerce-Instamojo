226aea55b570ee61765b4e70250d4ff207633560
Bhanu Prakash Poluparthi <32234113+BhanuPrakashNani@users.noreply.github.com>
2018-06-14 15:03:04 +0530

    Update details.php

--- src/Forms/refund/details.php
+++ src/Forms/refund/details.php
@@ -1,7 +1,22 @@
-try {
-    $response = $api->refundDetail('[REFUND ID]');
-    print_r($response);
-}
-catch (Exception $e) {
-    print('Error: ' . $e->getMessage());
-}
+<?php
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://www.instamojo.com/api/1.1/refunds/C5c0751272/');
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER,
+            array("X-Api-Key:d82016f839e13cd0a79afc0ef5b288b3",
+                  "X-Auth-Token:3827881f669c11e8dad8a023fd1108c2"));
+$payload = Array(
+    'payment_id' => 'MOJO5a06005J21512197',
+    'type' => 'QFL',
+    'body' => "Customer isn't satisfied with the quality"
+);
+$response = curl_exec($ch);
+curl_close($ch); 
+
+echo $response;
+
+?>
