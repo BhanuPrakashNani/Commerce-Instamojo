6219966e4cdfdbb2b6e6764646d407ec098e3799
Bhanu Prakash Poluparthi <32234113+BhanuPrakashNani@users.noreply.github.com>
2018-06-14 15:00:48 +0530

    Update create.php

--- src/Forms/refund/create.php
+++ src/Forms/refund/create.php
@@ -1,11 +1,24 @@
-try {
-    $response = $api->refundCreate(array(
-        'payment_id'=>'MOJO5c04000J30502939',
-        'type'=>'QFL',
-        'body'=>'Customer is not satified.'
-        ));
-    print_r($response);
-}
-catch (Exception $e) {
-    print('Error: ' . $e->getMessage());
-}
+<?php
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://www.instamojo.com/api/1.1/refunds/');
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER,
+            array("X-Api-Key:d82016f839e13cd0a79afc0ef5b288b3",
+                  "X-Auth-Token:3827881f669c11e8dad8a023fd1108c2"));
+$payload = Array(
+    'payment_id' => 'MOJO5a06005J21512197',
+    'type' => 'QFL',
+    'body' => "Customer isn't satisfied with the quality"
+);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($payload));
+$response = curl_exec($ch);
+curl_close($ch); 
+
+echo $response;
+
+?>
