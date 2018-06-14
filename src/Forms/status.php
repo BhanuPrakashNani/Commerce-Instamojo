d92d15b7095972f0aac522f8a7eb62b994a44496
Bhanu Prakash Poluparthi <32234113+BhanuPrakashNani@users.noreply.github.com>
2018-06-14 14:59:47 +0530

    Update status.php

--- src/Forms/status.php
+++ src/Forms/status.php
@@ -1,18 +1,17 @@
 <?php
 
-try {
-    $response = $api->paymentRequestStatus(['PAYMENT REQUEST ID']);
-    print_r($response);
-}
-catch (Exception $e) {
-    print('Error: ' . $e->getMessage());
-}
+$ch = curl_init();
 
-try {
-    $response = $api->paymentRequestPaymentStatus(['PAYMENT REQUEST ID'], ['PAYMENT ID']);
-    print_r($response['purpose']);  // print purpose of payment request
-    print_r($response['payment']['status']);  // print status of payment
-}
-catch (Exception $e) {
-    print('Error: ' . $e->getMessage());
-}
+curl_setopt($ch, CURLOPT_URL, 'https://www.instamojo.com/api/1.1/payment-requests/d66cb29dd059482e8072999f995c4eef/');
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER,
+            array("X-Api-Key:d82016f839e13cd0a79afc0ef5b288b3",
+                  "X-Auth-Token:3827881f669c11e8dad8a023fd1108c2"));
+$response = curl_exec($ch);
+curl_close($ch); 
+
+echo $response;
+
+?>
